709de58a268e3a3a0527d134d922c14340c3bf0c
JOEAI <42526993+jomarbp@users.noreply.github.com>
2025-09-30 16:58:46 -0500

    Update Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -8,7 +8,13 @@ function Home(){
     },[contador]);
     return(
         <div style={{ minHeight: '100vh', background: 'linear-gradient(135deg, #66aaeaff 0%, #764ba2 100%)'}}>
-            
+            <header className="bg-white shadow-sm">
+                <Container>
+                    <div className="d-flex justify-content-between align-item-center py-3">
+                        <h1 className="h3 mb-0 text-primary fw-bold">Jb Store</h1>
+                    </div>
+                </Container>
+            </header>
         </div>
     );
 }
